a893af0daef8a64ab8d2005ded8671f01440fd6a
Khyati Gupta <khyatigupta369@gmail.com>
2023-08-20 16:39:03 +0530

    views modified

--- tax_app/views.py
+++ tax_app/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+# tax_app/views.py
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
-from rest_framework import viewsets
-from tax_app.models import TaxationScheme
+# Import Models and Serialisers
+from .models import TaxationScheme
+from .serializers import TaxationSchemeSerializer
 
-class TaxViewSet(viewsets.ModelViewSet):
+# Views
+class FinancialYearList(generics.ListAPIView):
     queryset = TaxationScheme.objects.all()
-    serializer_class = TaxSerializer
+    serializer_class = TaxationSchemeSerializer
+
+class TaxCalculator(generics.CreateAPIView):
+    serializer_class = TaxationSchemeSerializer
+
+    def post(self, request, *args, **kwargs):
+        amount = request.data.get('amount')
+        year = request.data.get('year')
+        try:
+            taxation_scheme = TaxationScheme.objects.get(year=year)
+            tax = (taxation_scheme.tax_rate / 100) * amount
+            return Response({"tax_amount": tax}, status=status.HTTP_200_OK)
+        except TaxationScheme.DoesNotExist:
+            return Response({"error": "Taxation scheme not found for the specified year."}, status=status.HTTP_404_NOT_FOUND)
