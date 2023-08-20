279401842f7cbadac725d7ae341e72a3905a76bd
Khyati Gupta <khyatigupta369@gmail.com>
2023-08-20 23:24:43 +0530

    testing done post request working

--- tax_app/urls.py
+++ tax_app/urls.py
@@ -1,17 +1,4 @@
 from django.urls import path, include
-from tax_app.views import taxViewSet
-from rest_framework import routers
-
-# routers = routers.DefaultRouter()
-# routers.register(r'taxScheme', taxViewSet)
-
-
-# urlpatterns = [
-#     path('', include(routers.urls))
-# ]
-
-# tax_app/urls.py
-from django.urls import path
 from .views import FinancialYearList, TaxCalculator
 
 urlpatterns = [
--- tax_app/views.py
+++ tax_app/views.py
@@ -1,76 +1,29 @@
-# # tax_app/views.py
-# from rest_framework import generics, status
+# Import necessary modules
+from rest_framework import generics, status
 from rest_framework.response import Response
 
-# # Import Models and Serialisers
-# from .models import TaxationScheme
-# from .serializers import TaxationSchemeSerializer
+# Import models and serializers
+from .models import TaxationScheme
+from .serializers import TaxationSchemeSerializer
 
-# # Views
-# class FinancialYearList():
-#     queryset = TaxationScheme.objects.all()
-#     serializer_class = TaxationSchemeSerializer
-
-
-# class TaxCalculator(generics.CreateAPIView):
-#     serializer_class = TaxationSchemeSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         amount = request.data.get('amount')
-#         year = request.data.get('year')
-#         try:
-#             taxation_scheme = TaxationScheme.objects.get(year=year)
-#             tax = (taxation_scheme.tax_rate / 100) * amount
-#             return Response({"tax_amount": tax}, status=status.HTTP_200_OK)
-#         except TaxationScheme.DoesNotExist:
-#             return Response({"error": "Taxation scheme not found for the specified year."}, status=status.HTTP_404_NOT_FOUND)
-
-
-from decimal import Decimal  # Import the Decimal class
-from django.shortcuts import render
-from rest_framework import viewsets, generics, status
-from tax_app.models import TaxationScheme
-from tax_app.serializers import TaxationSchemeSerializer
-
-# class taxViewSet(viewsets.ModelViewSet):
-#     queryset = TaxationScheme.objects.all()
-#     serializer_class = TaxationSchemeSerializer
-
+# Define views
 class FinancialYearList(generics.ListAPIView):
     queryset = TaxationScheme.objects.all()
     serializer_class = TaxationSchemeSerializer
 
 class TaxCalculator(generics.CreateAPIView):
-    print("HIT the API")
     serializer_class = TaxationSchemeSerializer
 
-    def post(self, request, *args, **kwargs):
-        amount = request.data.get('amount')
+    def post(self, request):
+        print("Request data:", request.data)
+        amount = float(request.data.get('amount'))
         year = request.data.get('year')
+        print("Amount:", amount)
+        print("Year:", year)
         try:
             taxation_scheme = TaxationScheme.objects.get(year=year)
-            tax = (taxation_scheme.tax_rate / 100) * amount
+            print("Taxation Scheme:", taxation_scheme)
+            tax = (taxation_scheme.tax_rate * amount)/100
             return Response({"tax_amount": tax}, status=status.HTTP_200_OK)
         except TaxationScheme.DoesNotExist:
             return Response({"error": "Taxation scheme not found for the specified year."}, status=status.HTTP_404_NOT_FOUND)
-
-
-# class TaxCalculator(generics.CreateAPIView):
-#     print("enter")
-#     serializer_class = TaxationSchemeSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         amount = request.data.get('amount')
-#         year = request.data.get('year')
-#         try:
-#             taxation_scheme = TaxationScheme.objects.get(year=year)
-#             tax_rate = taxation_scheme.tax_rate  # Get the tax rate
-
-#             # Ensure both amount and tax_rate are Decimal objects
-#             if isinstance(amount, Decimal) and isinstance(tax_rate, Decimal):
-#                 tax = (tax_rate / 100) * amount
-#                 return Response({"tax_amount": tax}, status=status.HTTP_200_OK)
-#             else:
-#                 return Response({"error": "Invalid amount or tax rate."}, status=status.HTTP_400_BAD_REQUEST)
-#         except TaxationScheme.DoesNotExist:
-#             return Response({"error": "Taxation scheme not found for the specified year."}, status=status.HTTP_404_NOT_FOUND)
