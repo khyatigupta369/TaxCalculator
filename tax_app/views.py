c60c99d24cb58e1e3b0ebf18738a06e98a541e08
Khyati Gupta <khyatigupta369@gmail.com>
2023-08-20 16:31:09 +0530

    views set up

--- tax_app/views.py
+++ tax_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from tax_app.models import TaxationScheme
+
+class TaxViewSet(viewsets.ModelViewSet):
+    queryset = TaxationScheme.objects.all()
+    serializer_class = TaxSerializer
